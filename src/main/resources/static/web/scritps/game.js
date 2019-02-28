bef5da95e4ee13055d370a08eb45a516481b753c
Oriol Furnells Sanchez <oriol.furnells@gmail.com>
2019-02-28 14:28:09 +0100

    created game.js and game.html and style whith css

--- src/main/resources/static/web/scritps/game.js
+++ src/main/resources/static/web/scritps/game.js
@@ -1,28 +1,38 @@
 var app = new Vue({
     el: '#app',
     data: {
-        numbers: [1,2,3,4,5,6,7,8,9,10],
-        letters: ["A","B","C","D","E","F","G","H","I","J"]
+        numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+        gamePlayerId:"",
+        gameJsn: "",
+        gamePlayers:[],
+        shipsLocation:"",
+        cellMark:[],
+        gamePlayerIdPosCero:"",
+
     },
 
-    created:function(){
-    //     // console.log("hola");
-    //     this.runStart();
-    console.log("hola")
+    created: function () {
+    this.gameViewFetch();
     },
 
-    /*methods: {
-
-        runStart: function () {
-            fetch("http://localhost:8080/api/games", {
+    methods: {
+        gameViewFetch: function () { // con este fetch cojo el gamePlayerId del back para usarlo en el front
+            this.gamePlayerId= new URLSearchParams (window.location.search).get("gp");
+            fetch("http://localhost:8080/api/game_view/"+this.gamePlayerId, {
                 method: "GET",
             }).then(function (response) {
                 if (response.ok) {
                     return response.json();
                 }
             }).then(function (json) {
-                app.games= json;
-                console.log(app.games);
+                app.gameJsn= json;
+                app.shipsLocation= app.gameJsn.Ships;
+                app.gamePlayers= app.gameJsn.gamePlayers;
+                app.gamePlayerIdPosCero= app.gamePlayers[0].GamePlayerId;
+                console.log(app.gameJsn);
+                console.log(app.gamePlayers);
+                app.shipLocationsCell();
 
 
             }).catch(function (error) {
@@ -30,23 +40,55 @@ var app = new Vue({
             })
         },
 
+        shipLocationsCell: function(){
+            // let cell= [];
+            // for(let i =0; i<app.shipsLocation.length; i++){
+            //     cell.push(app.shipsLocation[i].ShipCell_Locations);
+            // }
+            app.cellMark=app.shipsLocation.flatMap(ship => ship.ShipCell_Locations);
+            // console.log(app.cellMark)
+        }
 
-        gamesList: function (){
-            let liElement=document.createElement("li");
-            let olElement=document.createElement("ol");
-            let divElement=document.createElement("div");
-
-
-
-
-
-            liElement.appendChild(olElement);
-            olElement.appendChild(divElement);
 
         }
 
 
-    },
+    // },
+
+    /*  runStart: function () {
+          fetch("http://localhost:8080/api/games", {
+              method: "GET",
+          }).then(function (response) {
+              if (response.ok) {
+                  return response.json();
+              }
+          }).then(function (json) {
+              app.games= json;
+              console.log(app.games);
+
+
+          }).catch(function (error) {
+              console.log("Request failed:" + error.message);
+          })
+      },
+
+
+      gamesList: function (){
+          let liElement=document.createElement("li");
+          let olElement=document.createElement("ol");
+          let divElement=document.createElement("div");
+
+
+
+
+
+          liElement.appendChild(olElement);
+          olElement.appendChild(divElement);
+
+      }
+
+
+  },
 */
 
 })
